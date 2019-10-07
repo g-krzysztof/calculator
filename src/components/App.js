7eee3f3e1ae7130c007f7e9ccedb20d687825206
Krzysztof Gryc <hello@krzysztofg.pl>
2019-10-07 22:04:08 +0200

    change App component to class component

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,17 @@ import React from 'react';
 import '../css/App.css';
 import Display from './Display';
 
-function App() {
-  return (
-    <div className="App">
-      <Display />
-    </div>
-  );
+class App extends React.Component {
+  state = {
+
+  }
+  render() {
+    return (
+      <div className="App">
+        <Display />
+      </div>
+    );
+  }
 }
 
 export default App;
