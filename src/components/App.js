ae208e90811a74646dea6f0c62b4a0686531b1e2
Krzysztof Gryc <hello@krzysztofg.pl>
2019-10-07 21:58:00 +0200

    add Display component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import '../css/App.css';
+import Display from './Display';
 
 function App() {
   return (
     <div className="App">
-      Hello world!
+      <Display />
     </div>
   );
 }
---
+++ src/components/Display.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../css/Display.css';
+
+function Display() {
+    return (
+        <div className="Display">
+            Display
+    </div>
+    );
+}
+
+export default Display;
