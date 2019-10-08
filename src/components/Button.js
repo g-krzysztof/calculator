5e51069dda7bce889447b511016c4ca623dea369
Krzysztof Gryc <hello@krzysztofg.pl>
2019-10-08 10:01:50 +0200

    add buttons styles

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,7 +4,7 @@ import '../css/Button.css';
 function Button(props) {
     return (
         <div className="Button">
-            <button value={props.name} onClick={props.onClick}>{props.name}</button>
+            <button className="Button__btn" value={props.name} onClick={props.onClick}>{props.name}</button>
         </div>
     );
 }
